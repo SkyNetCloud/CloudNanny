099cdc2a3d1ac3161fba33cffe0910a6a881a591
SkyNetCloud <mcboxer1201@gmail.com>
2023-08-10 13:49:15 -0400

    More Sign update

--- code/signin.php
+++ code/signin.php
@@ -82,7 +82,7 @@ function createToken($mysqli, $user_id, $name, $id, $username, $module_type) {
 }
 
 function createRedstoneEntry($mysqli, $token, $id) {
-	$query = "INSERT INTO redstone_controls (token, computer_id) VALUES ('$token', "$id")";
+	$query = "INSERT INTO redstone_controls (token, computer_id) VALUES ('$token', "$idstring")";
 	$result = $mysqli->query($query);
 }
 
@@ -92,7 +92,7 @@ function createTankEntry($mysqli, $token, $id) {
 }
 
 function createEnergyEntry($mysqli, $token, $id) {
-	$query = "INSERT INTO energy_storage (token, computer_id) VALUES ('$token', '$id')";
+	$query = "INSERT INTO energy_storage (token, computer_id) VALUES ('$token', '$idstring')";
 	$result = $mysqli->query($query);
 }
 
