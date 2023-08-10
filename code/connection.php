33ce57a16c29aaa57066ba4b5b3621d325864ad9
SkyNetCloud <mcboxer1201@gmail.com>
2023-08-10 16:07:27 -0400

    Update

--- code/connection.php
+++ code/connection.php
@@ -4,7 +4,4 @@
 $dbConn = mysql_connect('127.0.0.1', 'SkyNetCloud', 'SkyNetCloud#')
 	or die(print_r(mysql_error()));
 mysql_select_db('cloudnanny') or die(print_r(mysql_error()));
-r($mysqli)));
-($mysqli)));
-
 ?>
