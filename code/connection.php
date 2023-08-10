b001cc0b8738ee5ec5245d5aea705fcfff2aef9e
SkyNetCloud <mcboxer1201@gmail.com>
2023-08-10 00:46:52 -0400

    last update

--- code/connection.php
+++ code/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 //open database connection
-$dbConn = mysqli_connect('localhost', 'cloudpc', '-Ke[RG(XqO1q8qIe','cloudnanny') or die(print_r(mysql_error()));
-mysqli_select_db($dbConn 'cloudnanny') or die(print_r(mysql_error()));
+$dbConn = mysqli_connect('localhost', 'cloudpc', '-Ke[RG(XqO1q8qIe', 'cloudnanny') or die(print_r(mysql_error()));
+mysqli_select_db($dbConn, 'cloudnanny') or die(print_r(mysql_error()));
 
 ?>
