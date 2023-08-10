c9e84e732c3ce6dc28bf2a889f484039396d7ef4
SkyNetCloud <mcboxer1201@gmail.com>
2023-08-10 00:46:01 -0400

    Code update again

--- code/connection.php
+++ code/connection.php
@@ -2,6 +2,6 @@
 
 //open database connection
 $dbConn = mysqli_connect('localhost', 'cloudpc', '-Ke[RG(XqO1q8qIe','cloudnanny') or die(print_r(mysql_error()));
-mysql_select_db('cloudnanny') or die(print_r(mysql_error()));
+mysqli_select_db($dbConn 'cloudnanny') or die(print_r(mysql_error()));
 
 ?>
