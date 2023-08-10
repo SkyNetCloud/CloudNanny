746e33047e5dfa9c14540803540142458fcc4a9a
SkyNetCloud <mcboxer1201@gmail.com>
2023-08-10 00:56:16 -0400

    hmm

--- code/connection.php
+++ code/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 //open database connection
-$dbConn = mysqli_connect('localhost', 'cloudpc', '-Ke[RG(XqO1q8qIe', 'cloudnanny') or die(print_r(mysql_error()));
-mysqli_select_db($dbConn, 'cloudnanny') or die(print_r(mysql_error()));
+$dbConn = mysql_connect('localhost', 'cloudpc', '-Ke[RG(XqO1q8qIe', 'cloudnanny') or die(print_r(mysql_error()));
+mysql_select_db("cloudnanny", $dbConn) or die(print_r(mysql_error()));
 
 ?>
