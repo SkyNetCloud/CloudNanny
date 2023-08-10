b615a4a9f2cbd856faa067f5f22985a4d2636da9
SkyNetCloud <mcboxer1201@gmail.com>
2023-08-09 23:57:15 -0400

    More Database Update

--- code/connection.php
+++ code/connection.php
@@ -3,10 +3,11 @@
 $servername =  "192.168.0.62"
 $username = "cloudnanny"
 $password = "-Ke[RG(XqO1q8qIe"
+$databaseName = "cloudnanny" 
 
 
 //open database connection
-$dbConn = mysql_connect($servername, $username, $password)
+$dbConn = mysqli_connect($servername, $username, $password, $databaseName)
 	or die(print_r(mysql_error()));
 mysql_select_db('') or die(print_r(mysql_error()));
 
