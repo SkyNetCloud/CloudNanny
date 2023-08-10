048350be6965efad7460d185262189520c5cddbc
SkyNetCloud <mcboxer1201@gmail.com>
2023-08-10 00:31:54 -0400

    Update Code

--- code/connection.php
+++ code/connection.php
@@ -8,5 +8,4 @@ $dbpass = '-Ke[RG(XqO1q8qIe';
 $dbhost = 'localhost';
 
 $link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbh>
-mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
-?>
+mysqli_select_db($link, $dbname);
