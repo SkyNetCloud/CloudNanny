69f480637ea791421f6fbfb6c5b192ee8941bee5
SkyNetCloud <mcboxer1201@gmail.com>
2023-08-10 13:28:40 -0400

    Update Code

--- code/energy.php
+++ code/energy.php
@@ -13,26 +13,29 @@ $bat_name = $_POST['bat_name'];
 $energy_type = $_POST['energy_type'];
 $percent = $_POST['percent'];
 
+
+$tokenstring = $mysqli->real_escape_string($this->$token);
+$idstring = $mysqli->real_escape_string($this->$id);
+$batnamestring = $mysqli->real_escape_string($this->$bat_name);
+$energytypestring = $mysqli->real_escape_string($this->$token);
+$percentstring = $mysqli->real_escape_string($this->$token);
+
+
+
 $energy_type = htmlspecialchars($energy_type);
 $bat_name = htmlspecialchars($bat_name);
 $percent = htmlspecialchars($percent);
 
-$query = "UPDATE tokens SET last_seen = NOW() WHERE token = '".dbEsc($token)."' AND computer_id = ".dbEsc($id);
+$query = "UPDATE tokens SET last_seen = NOW() WHERE token = '$tokenstring' AND computer_id = '$'");
 $result = $mysqli->query($query);
 
 if ($result) {
-	$query2 = "UPDATE energy_storage SET bat_name = '".dbEsc($bat_name)."', energy_type = '".dbEsc($energy_type)."', percent = '".dbEsc($percent)."' WHERE token = '".dbEsc($token)."'";
+	$query2 = "UPDATE energy_storage SET bat_name = '$bat_name', energy_type = '$energytypestring', percent = '"$percentstring"' WHERE token = '"$tokenstring."'";
 	$result2 = $mysqli->query($query2);
 
 	echo $version;
 } else {
 	echo 'error: token update query failed.';
-}
-
-
-function dbEsc($theString) {
-	$theString = $mysqli->real_escape_string($theString);
-	return $theString;
 }
 
 ?>
