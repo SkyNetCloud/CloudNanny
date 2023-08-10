9188546bb35f9683821e6719d17d4e67e2f46915
SkyNetCloud <mcboxer1201@gmail.com>
2023-08-10 10:46:28 -0400

    Error checking

--- code/connection.php
+++ code/connection.php
@@ -1,7 +1,17 @@
 <?php
+session_start();
+
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'phplogin';
 
 //open database connection
 $dbConn = mysqli_connect('localhost', 'SkyNetCloud', 'SkyNetCloud#', 'cloudnanny') or die(print_r(mysqli_error($dbConn)));
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
 mysqli_select_db($dbConn,"cloudnanny") or die(print_r(mysqli_error($dbConn)));
 
 ?>
